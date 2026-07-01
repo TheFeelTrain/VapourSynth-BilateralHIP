2f908291f7829128185e7a8f31704570e3a34168
TheFeelTrain <the@feeltra.in>
2026-07-01 04:12:38 -0700

    Convert to API v4

--- source/bilateral.cpp
+++ source/bilateral.cpp
@@ -14,8 +14,8 @@
 
 #include <hip/hip_runtime.h>
 
-#include <VapourSynth.h>
-#include <VSHelper.h>
+#include <VapourSynth4.h>
+#include <VSHelper4.h>
 
 #include <config.h>
 
@@ -113,8 +113,8 @@ struct HIP_Resource {
 };
 
 struct BilateralData {
-    VSNodeRef * node;
-    VSNodeRef * ref_node;
+    VSNode * node;
+    VSNode * ref_node;
     const VSVideoInfo * vi;
 
     int device_id, num_streams;
@@ -126,19 +126,11 @@ struct BilateralData {
     std::mutex resources_lock;
 };
 
-static void VS_CC BilateralInit(
-    VSMap *in, VSMap *out, void **instanceData, VSNode *node,
-    VSCore *core, const VSAPI *vsapi) {
-
-    BilateralData * d = static_cast<BilateralData *>(*instanceData);
-    vsapi->setVideoInfo(d->vi, 1, node);
-}
-
-static const VSFrameRef *VS_CC BilateralGetFrame(
-    int n, int activationReason, void **instanceData, void **frameData,
+static const VSFrame *VS_CC BilateralGetFrame(
+    int n, int activationReason, void *instanceData, void **frameData,
     VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi) {
 
-    BilateralData * d = static_cast<BilateralData *>(*instanceData);
+    BilateralData * d = static_cast<BilateralData *>(instanceData);
 
     if (activationReason == arInitial) {
         vsapi->requestFrameFilter(n, d->node, frameCtx);
@@ -146,21 +138,21 @@ static const VSFrameRef *VS_CC BilateralGetFrame(
             vsapi->requestFrameFilter(n, d->ref_node, frameCtx);
         }
     } else if (activationReason == arAllFramesReady) {
-        const VSFrameRef * src = vsapi->getFrameFilter(n, d->node, frameCtx);
-        const VSFrameRef * ref = nullptr;
+        const VSFrame * src = vsapi->getFrameFilter(n, d->node, frameCtx);
+        const VSFrame * ref = nullptr;
         if (d->ref_node) {
             ref = vsapi->getFrameFilter(n, d->ref_node, frameCtx);
         }
 
         const int pl[] = { 0, 1, 2 };
-        const VSFrameRef * fr[] = {
+        const VSFrame * fr[] = {
             d->process[0] ? nullptr : src,
             d->process[1] ? nullptr : src,
             d->process[2] ? nullptr : src
         };
 
-        VSFrameRef * dst = vsapi->newVideoFrame2(
-            d->vi->format, d->vi->width, d->vi->height, fr, pl, src, core);
+        VSFrame * dst = vsapi->newVideoFrame2(
+            &d->vi->format, d->vi->width, d->vi->height, fr, pl, src, core);
 
         d->semaphore.acquire();
         d->resources_lock.lock();
@@ -185,7 +177,7 @@ static const VSFrameRef *VS_CC BilateralGetFrame(
         hipStream_t stream = resource.stream;
         const auto & graphexecs = resource.graphexecs;
 
-        for (int plane = 0; plane < d->vi->format->numPlanes; plane++) {
+        for (int plane = 0; plane < d->vi->format.numPlanes; plane++) {
             if (!d->process[plane]) {
                 continue;
             }
@@ -194,7 +186,7 @@ static const VSFrameRef *VS_CC BilateralGetFrame(
             int height = vsapi->getFrameHeight(src, plane);
 
             int s_pitch = vsapi->getStride(src, plane);
-            int bps = d->vi->format->bitsPerSample;
+            int bps = d->vi->format.bitsPerSample;
             int s_stride = s_pitch / (bps / 8);
             int width_bytes = width * sizeof(float);
             auto srcp = vsapi->getReadPtr(src, plane);
@@ -207,9 +199,9 @@ static const VSFrameRef *VS_CC BilateralGetFrame(
             }
 
             if (bps == 32) {
-                vs_bitblt(h_buffer, d_pitch, srcp, s_pitch, width_bytes, height);
+                vsh::bitblt(h_buffer, d_pitch, srcp, s_pitch, width_bytes, height);
                 if (d->ref_node) {
-                    vs_bitblt(&h_buffer[s_stride * height], d_pitch, refp, s_pitch, width_bytes, height);
+                    vsh::bitblt(&h_buffer[s_stride * height], d_pitch, refp, s_pitch, width_bytes, height);
                 }
             } else if (bps == 16) {
                 float * h_bufferp = h_buffer;
@@ -253,7 +245,7 @@ static const VSFrameRef *VS_CC BilateralGetFrame(
             auto dstp = vsapi->getWritePtr(dst, plane);
 
             if (bps == 32) {
-                vs_bitblt(dstp, s_pitch, h_buffer, d_pitch, width_bytes, height);
+                vsh::bitblt(dstp, s_pitch, h_buffer, d_pitch, width_bytes, height);
             } else if (bps == 16) {
                 uint16_t * dst16p = reinterpret_cast<uint16_t *>(dstp);
                 const float * h_bufferp = h_buffer;
@@ -318,16 +310,16 @@ static void VS_CC BilateralCreate(
 
     auto d { std::make_unique<BilateralData>() };
 
-    d->node = vsapi->propGetNode(in, "clip", 0, nullptr);
+    d->node = vsapi->mapGetNode(in, "clip", 0, nullptr);
     d->vi = vsapi->getVideoInfo(d->node);
 
     int error;
 
-    d->ref_node = vsapi->propGetNode(in, "ref", 0, &error);
+    d->ref_node = vsapi->mapGetNode(in, "ref", 0, &error);
     bool has_ref = d->ref_node != nullptr;
 
     auto set_error = [&](const std::string & error_message) {
-        vsapi->setError(out, ("BilateralGPU: " + error_message).c_str());
+        vsapi->mapSetError(out, ("BilateralGPU: " + error_message).c_str());
         if (has_ref) {
             vsapi->freeNode(d->ref_node);
         }
@@ -335,10 +327,10 @@ static void VS_CC BilateralCreate(
     };
 
     if (auto [bps, sample] = std::pair{
-            d->vi->format->bitsPerSample,
-            d->vi->format->sampleType
+            d->vi->format.bitsPerSample,
+            d->vi->format.sampleType
         };
-        !isConstantFormat(d->vi) ||
+        !vsh::isConstantVideoFormat(d->vi) ||
         (sample == stInteger && (bps != 8 && bps != 16)) ||
         (sample == stFloat && bps != 32)
     ) {
@@ -347,21 +339,21 @@ static void VS_CC BilateralCreate(
     }
 
     const auto ref_vi = vsapi->getVideoInfo(d->ref_node);
-    if (d->ref_node && (!isSameFormat(d->vi, ref_vi) || d->vi->numFrames != ref_vi->numFrames)) {
+    if (d->ref_node && (!vsh::isSameVideoInfo(d->vi, ref_vi) || d->vi->numFrames != ref_vi->numFrames)) {
         return set_error("\"ref\" must be of the same format as \"clip\"");
     }
 
     std::array<float, 3> sigma_spatial;
     for (int i = 0; i < std::ssize(sigma_spatial); ++i) {
         sigma_spatial[i] = static_cast<float>(
-            vsapi->propGetFloat(in, "sigma_spatial", i, &error));
+            vsapi->mapGetFloat(in, "sigma_spatial", i, &error));
 
         if (error) {
             if (i == 0) {
                 sigma_spatial[i] = 3.0f;
             } else if (i == 1) {
-                auto subH = d->vi->format->subSamplingH;
-                auto subW = d->vi->format->subSamplingW;
+                auto subH = d->vi->format.subSamplingH;
+                auto subW = d->vi->format.subSamplingW;
                 sigma_spatial[i] = static_cast<float>(
                     sigma_spatial[0] / std::sqrt((1 << subH) * (1 << subW)));
             } else {
@@ -384,7 +376,7 @@ static void VS_CC BilateralCreate(
     std::array<float, 3> sigma_color;
     for (int i = 0; i < std::ssize(sigma_color); ++i) {
         sigma_color[i] = static_cast<float>(
-            vsapi->propGetFloat(in, "sigma_color", i, &error));
+            vsapi->mapGetFloat(in, "sigma_color", i, &error));
 
         if (error) {
             if (i == 0) {
@@ -408,7 +400,7 @@ static void VS_CC BilateralCreate(
 
     std::array<int, 3> radius;
     for (int i = 0; i < std::ssize(radius); ++i) {
-        radius[i] = int64ToIntS(vsapi->propGetInt(in, "radius", i, &error));
+        radius[i] = vsh::int64ToIntS(vsapi->mapGetInt(in, "radius", i, &error));
 
         if (error) {
             radius[i] = std::max(1, static_cast<int>(std::roundf(sigma_spatial[i] * 3.f)));
@@ -417,7 +409,7 @@ static void VS_CC BilateralCreate(
         }
     }
 
-    int device_id = int64ToIntS(vsapi->propGetInt(in, "device_id", 0, &error));
+    int device_id = vsh::int64ToIntS(vsapi->mapGetInt(in, "device_id", 0, &error));
     if (error) {
         device_id = 0;
     }
@@ -431,12 +423,12 @@ static void VS_CC BilateralCreate(
     }
     d->device_id = device_id;
 
-    d->num_streams = int64ToIntS(vsapi->propGetInt(in, "num_streams", 0, &error));
+    d->num_streams = vsh::int64ToIntS(vsapi->mapGetInt(in, "num_streams", 0, &error));
     if (error) {
         d->num_streams = 4;
     }
 
-    bool use_shared_memory = !!vsapi->propGetInt(in, "use_shared_memory", 0, &error);
+    bool use_shared_memory = !!vsapi->mapGetInt(in, "use_shared_memory", 0, &error);
     if (error) {
         use_shared_memory = true;
     }
@@ -448,8 +440,8 @@ static void VS_CC BilateralCreate(
 
         int width = d->vi->width;
         int height = d->vi->height;
-        int ssw = d->vi->format->subSamplingW;
-        int ssh = d->vi->format->subSamplingH;
+        int ssw = d->vi->format.subSamplingW;
+        int ssh = d->vi->format.subSamplingH;
 
         int max_width { d->process[0] ? width : width >> ssw };
         int max_height { d->process[0] ? height : height >> ssh };
@@ -475,7 +467,7 @@ static void VS_CC BilateralCreate(
             checkError(hipStreamCreateWithFlags(&stream.data, hipStreamNonBlocking));
 
             std::array<Resource<hipGraphExec_t, hipGraphExecDestroy>, 3> graphexecs {};
-            for (int plane = 0; plane < d->vi->format->numPlanes; ++plane) {
+            for (int plane = 0; plane < d->vi->format.numPlanes; ++plane) {
                 if (!d->process[plane]) {
                     continue;
                 }
@@ -501,32 +493,47 @@ static void VS_CC BilateralCreate(
         }
     }
 
-    vsapi->createFilter(
-        in, out, "Bilateral",
-        BilateralInit, BilateralGetFrame, BilateralFree,
-        fmParallel, 0, d.release(), core);
+    VSFilterDependency deps[2] = {{d->node, rpStrictSpatial}};
+    int num_deps = 1;
+    if (has_ref) {
+        deps[1].source = d->ref_node;
+        deps[1].requestPattern = rpStrictSpatial;
+        num_deps = 2;
+    }
+
+    vsapi->createVideoFilter(
+        out, "Bilateral", d->vi,
+        BilateralGetFrame, BilateralFree,
+        fmParallel, deps, num_deps, d.release(), core);
 }
 
-VS_EXTERNAL_API(void) VapourSynthPluginInit(
-    VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
+VS_EXTERNAL_API(void)
+VapourSynthPluginInit2(VSPlugin *plugin, const VSPLUGINAPI *vspapi) {
+    vspapi->configPlugin(
+        "com.thefeeltrain.bilateralhip",
+        "bilateralhip",
+        "Bilateral filter using HIP",
+        VS_MAKE_VERSION(1, 0),
+        VAPOURSYNTH_API_VERSION, 0, plugin
+    );
 
-    configFunc(
-        "com.thefeeltrain.bilateralhip", "bilateralhip", "Bilateral filter using HIP",
-        VAPOURSYNTH_API_VERSION, 1, plugin);
-
-    registerFunc("Bilateral",
-        "clip:clip;"
+    vspapi->registerFunction(
+        "Bilateral",
+        "clip:vnode;"
         "sigma_spatial:float[]:opt;"
         "sigma_color:float[]:opt;"
         "radius:int[]:opt;"
         "device_id:int:opt;"
         "num_streams:int:opt;"
         "use_shared_memory:int:opt;"
-        "ref:clip:opt;",
-        BilateralCreate, nullptr, plugin);
+        "ref:vnode:opt;",
+        "clip:vnode;",
+        BilateralCreate, nullptr, plugin
+    );
 
-    auto getVersion = [](const VSMap *, VSMap * out, void *, VSCore *, const VSAPI *vsapi) {
-        vsapi->propSetData(out, "version", VERSION, -1, paReplace);
+    auto getVersion = [](const VSMap *, VSMap *out, void *, VSCore *,const VSAPI *vsapi) {
+        vsapi->mapSetData(out, "version", VERSION, -1, dtUtf8, maReplace);
     };
-    registerFunc("Version", "", getVersion, nullptr, plugin);
+
+    vspapi->registerFunction("Version", "", "any", getVersion, nullptr, plugin);
 }
